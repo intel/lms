intel/lms

commit 59e7df512c11066b218cd24e04a45275642e42b4

LMS: prevent LMS failing while stopping the service
In case that the operation already done for all services,
and no operation occurred here, we should return false,
for TaskCompleted to be called.

Signed-off-by: Riki Sharon <riki.sharon@intel.com>
Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

UNS/Configurator/ServicesBatchCommand.cpp
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2010-2015 Intel Corporation
+ * Copyright (C) 2010-2019 Intel Corporation
  */
 #include "ServicesBatchCommand.h"
 #include "DependancyManager.h"
@@ -19,6 +19,8 @@ IServicesManager *ServicesBatchCommand::s_servicesManager = NULL;
 bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 {
 	bool ret = true;
+	bool operationAlreadyDoneForAllServices = true;
+
 	NamesList serviceNames;
 	services.GetNames(serviceNames);
 	NamesList::const_iterator endIt = serviceNames.end();
@@ -28,6 +30,8 @@ bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 		//if service is already in requested state - return true
 		if (OperationAlreadyDone(service))
 			continue;
+
+		operationAlreadyDoneForAllServices = false;
 		// if there are dependencies - call the command on them. the configurator will call 
 		// "execute" again when the services that should have been completing the command before, 
 		// have finished, and then execute will be called again.
@@ -46,6 +50,11 @@ bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 				ret = false;
 			}
 		}
+	}
+
+	if (operationAlreadyDoneForAllServices)
+	{
+		ret = false; // in case that the operation already done for all services, and no operation occured here, we should return false, for TaskCompleted to be called
 	}
 
 	return ret;

commit feb0e9a75d9eb2a36880efee2e6ccacab9be7cc6

LMS: Configurator: distinguish between failed and void Execution
The fix 'LMS: prevent LMS failing while stopping the service'
changed behavior of Execute() return false in case of all operations
has already done their job in order to trigger TaskCompleted().

This has, however created an issue since Execute() performs
recursive calls on the dependencies. In those inner calls
we should be able to distinguish between failure and completed
jobs, so we don't propagate completed jobs as failures, and trigger
TaskCompleted().

Fixes: 59e7df512c11 (LMS: prevent LMS failing while stopping the service)
Signed-off-by: Riki Sharon <riki.sharon@intel.com>
Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

UNS/Configurator/Configurator.cpp
@@ -731,7 +731,7 @@ void Configurator::StopAllServices(bool stopMainService)
 	theLoadedServices::instance()->GetAllLoadedServices(remainingServices);
 	if(!remainingServices.empty())
 	{
-		if(!stopMisc.Execute(remainingServices))
+		if(stopMisc.Execute(remainingServices) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 			TaskCompleted();
 	}
 	else if(stopMainService)
@@ -751,7 +751,7 @@ bool Configurator::SuspendResumeAllServices(const ServicesBatchCommand &command)
 
 	NamesList loadedServices;
 	theLoadedServices::instance()->GetAllLoadedServices(loadedServices);
-	if(!command.Execute(loadedServices))
+	if(command.Execute(loadedServices) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 	{
 		TaskCompleted();
 		return false;
@@ -888,7 +888,7 @@ void Configurator::ScanConfiguration()
 		theDependencyManager::instance()->FillAbsoluteDependencies(servicesNames);
 		theLoadedServices::instance()->SetServicesToLoad(servicesNames);
 
-		if(!batch.Execute(servicesNames))
+		if(batch.Execute(servicesNames) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 			TaskCompleted();
 
 		ACE_Reactor::instance()->cancel_timer (this);
@@ -930,7 +930,7 @@ void Configurator::OnToggleService(const ACE_TString &service, bool val)
 	{
 		theLoadedServices::instance()->AddServiceToLoad(service);
 		ServicesBatchStartCommand	batchStartCommand;
-		if(!batchStartCommand.Execute(services))
+		if(batchStartCommand.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 		{
 			TaskCompleted();
 		}
@@ -944,7 +944,7 @@ void Configurator::OnToggleService(const ACE_TString &service, bool val)
 
 		theLoadedServices::instance()->RemoveServiceToLoad(service);
 		ServicesBatchStopCommand	batchStopCommand;
-		if(!batchStopCommand.Execute(services))
+		if(batchStopCommand.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 			TaskCompleted();
 	}
 }
@@ -964,7 +964,7 @@ void Configurator::ChangeServiceState(ACE_TString &serviceName, int status)
 			{
 				//stopping all the services that were in waiting list.
 				ServicesBatchStopCommand command;
-				if(!command.Execute(services))
+				if(command.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 					TaskCompleted();
 			}
 			else if(serviceName.compare(FIRST_SERVICE) == 0)
@@ -987,7 +987,7 @@ void Configurator::ChangeServiceState(ACE_TString &serviceName, int status)
 			{
 				//starting all the services that were in waiting list.
 				ServicesBatchStartCommand command;
-				if(!command.Execute(services))
+				if(command.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 					TaskCompleted();
 			}
 			else if(serviceName.compare(LAST_SERVICE) == 0 || serviceName.compare(AMT_ENABLE_LAST_SERVICE) == 0 || serviceName.compare(WAITING_FOR_PFW_LAST_SERVICE) == 0)
@@ -1004,7 +1004,7 @@ void Configurator::ChangeServiceState(ACE_TString &serviceName, int status)
 			{
 				// Suspending all the services that were in waiting list.
 				ServicesBatchSuspendCommand command;
-				if(!command.Execute(services))
+				if(command.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 					TaskCompleted();
 			}
 			else if(serviceName.compare(FIRST_SERVICE) == 0)
@@ -1020,7 +1020,7 @@ void Configurator::ChangeServiceState(ACE_TString &serviceName, int status)
 			{
 				// Resuming all the services that were in waiting list.
 				ServicesBatchResumeCommand command;
-				if(!command.Execute(services))
+				if(command.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 					TaskCompleted();
 			}
 			else if(serviceName.compare(LAST_SERVICE) == 0)
@@ -1074,7 +1074,7 @@ int Configurator::UpdateConfiguration(const ChangeConfiguration *conf)
 						theLoadedServices::instance()->AddServiceToLoad(*it);
 
 					ServicesBatchStartCommand batch;
-					if(!batch.Execute(services))
+					if(batch.Execute(services) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 						TaskCompleted();
 				}
 				else
@@ -1107,7 +1107,7 @@ int Configurator::UpdateConfiguration(const ChangeConfiguration *conf)
 				}
 				
 				ServicesBatchStartCommand batch;
-				if (!batch.Execute(servicesNames))
+				if (batch.Execute(servicesNames) != ServicesBatchCommand::ExecuteCommandResult::SUCCESS)
 				{
 					UNS_ERROR(L"Starting the services that waited for Port forwarding to start - Failed.\n");
 					TaskCompleted();

UNS/Configurator/ServicesBatchCommand.cpp
@@ -12,14 +12,12 @@ IServicesManager *ServicesBatchCommand::s_servicesManager = NULL;
 /* This function executes the command, on all given services.           */
 /* For each service, the function checks if there are dependencies that */
 /* were not fulfilled yet, and if there are, it recursively executes the*/
-/* command on them. If there are no dependencies, it executes the		*/
-/* command on the service.												*/ 
-
+/* command on them. If there are no dependencies, it executes the       */
+/* command on the service.                                              */
 /************************************************************************/
-bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
+ServicesBatchCommand::ExecuteCommandResult ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 {
-	bool ret = true;
-	bool operationAlreadyDoneForAllServices = true;
+	ExecuteCommandResult ret = NOTHING_TO_DO;
 
 	NamesList serviceNames;
 	services.GetNames(serviceNames);
@@ -31,15 +29,21 @@ bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 		if (OperationAlreadyDone(service))
 			continue;
 
-		operationAlreadyDoneForAllServices = false;
-		// if there are dependencies - call the command on them. the configurator will call 
-		// "execute" again when the services that should have been completing the command before, 
+		if (ret != FAILURE)
+		{
+			ret = SUCCESS;
+		}
+
+		// if there are dependencies - call the command on them. the configurator will call
+		// "execute" again when the services that should have been completing the command before,
 		// have finished, and then execute will be called again.
 		if (!theAsyncActivationManager::instance()->ReadyToOperation(service, GetOperation()))
 		{
 			ServiceNamesList dependencies;
 			GetPrerequisites(service, dependencies);
-			ret &= Execute(dependencies);
+
+			if (Execute(dependencies) == FAILURE)
+				ret = FAILURE;
 		}
 		else //no dependencies - let's do the requested operation!
 		{
@@ -47,14 +51,9 @@ bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 			if(!DoOperation(service))
 			{
 				theLoadedServices::instance()->UnlockService(service);
-				ret = false;
+				ret = FAILURE;
 			}
 		}
-	}
-
-	if (operationAlreadyDoneForAllServices)
-	{
-		ret = false; // in case that the operation already done for all services, and no operation occured here, we should return false, for TaskCompleted to be called
 	}
 
 	return ret;
@@ -63,7 +62,7 @@ bool ServicesBatchCommand::Execute(const ServiceNamesList &services) const
 bool ServicesBatchCommand::OperationAlreadyDone(const ACE_TString &service) const
 {
 	return theLoadedServices::instance()->IsLocked(service) ||	//if the service is already in transition from state to state
-		   DoneCriteria(service);								// or the specific state already reached 
+		   DoneCriteria(service);								// or the specific state already reached
 }
 
 void ServicesBatchCommand::fillDependencies(const ACE_TString &service, ServiceNamesList &dependencies, bool serviceDependsOn) const

UNS/Configurator/ServicesBatchCommand.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2011-2015 Intel Corporation
+ * Copyright (C) 2011-2019 Intel Corporation
  */
 #ifndef __SERVICES_BATCH_COMMAND_H_
 #define __SERVICES_BATCH_COMMAND_H_
@@ -13,12 +13,13 @@
 class ServicesBatchCommand
 {
 public:
+	typedef enum { SUCCESS, NOTHING_TO_DO, FAILURE } ExecuteCommandResult;
 	virtual ~ServicesBatchCommand() {}
 	
 	static void SetServicesManager(IServicesManager *servicesManager)
 		{s_servicesManager = servicesManager;}
 
-	bool Execute(const ServiceNamesList &services) const;
+	ExecuteCommandResult Execute(const ServiceNamesList &services) const;
 
 protected:
 