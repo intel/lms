7c05ccb0485d9d4abc167ba2ce00ae67e8088cb5
Alexander Usyskin <alexander.usyskin@intel.com>
2021-01-31 19:15:20 +0200

    LMS: obtain libxml2 and ACE from Conan
    Obtain libxml2 and ACE from a Conan repository.
    Remove ace-config* files as we dropped the infrastructure
    for building ACE alongside with LMS.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2021 Intel Corporation
+from conans import ConanFile
+import os
+
+class LMSConan(ConanFile):
+    name = "lms"
+    generators = "cmake", "cmake_find_package", "visual_studio"
+    settings = "os"
+
+    def requirements(self):
+        self.requires("metee/3.0.0@mesw/stable")
+        if self.settings.os == "Windows":
+            self.requires("libxml2/2.9.10@mesw/stable")
+            self.requires("ACE/6.5.11@mesw/stable")
+        else:
+            if os.environ.get("LOCAL_ACE", None):
+                self.requires("ACE/6.5.11@mesw/stable")
