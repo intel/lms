d9262e9f87aa64921301b4a36abd3dd60fc0901e
Alexander Usyskin <alexander.usyskin@intel.com>
2021-09-14 15:59:12 +0300

    LMS: snap: drop not needed connect to mei plug
    Mei plug now defined as auto-connect for LMS in snap store.
    Drop no more needed manual plug line from script.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- snap/local/connect.sh
+++ snap/local/connect.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2019-2021 Intel Corporation
+
 sudo snap connect lms:shutdown :shutdown
 sudo snap connect lms:network-observe :network-observe
 sudo snap connect lms:network-manager :network-manager
 sudo snap connect lms:hardware-observe :hardware-observe
 sudo snap connect lms:network-control :network-control
-sudo snap connect lms:intel-mei :intel-mei
