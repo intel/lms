df66841f4dd685a2845aee85836e19b1ea8d7fc6
Alexander Usyskin <alexander.usyskin@intel.com>
2022-03-06 13:04:15 +0200

    LMS: DataInterfaceBackend: drop unused include
    Drop unused SMBIOS_Reader.h include.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- UNS/DataInterfaceBackend/AT_Device_BE.cpp
+++ UNS/DataInterfaceBackend/AT_Device_BE.cpp
@@ -1,10 +1,9 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2009-2020 Intel Corporation
+ * Copyright (C) 2009-2022 Intel Corporation
  */
 #include "AT_Device_BE.h"
 #include "global.h"
-#include "SMBIOS_Reader.h"
 #include "AuditLogWSManClient.h"
 #include <vector>
 
