11fd80e35425202de9472aff31b0d6f891331699
Alexander Usyskin <alexander.usyskin@intel.com>
2023-01-19 08:38:13 +0200

    LMS: WsmanClient: no need to init on creation
    Variable is always intialized in flow.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- WsmanClient/EthernetSettingsWSManClient.cpp
+++ WsmanClient/EthernetSettingsWSManClient.cpp
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2009-2020 Intel Corporation
+ * Copyright (C) 2009-2023 Intel Corporation
  */
 /*++
 
@@ -26,7 +26,7 @@ EthernetSettingsWSManClient::~EthernetSettingsWSManClient()
 
 unsigned int EthernetSettingsWSManClient::Enumerate(std::vector<std::shared_ptr<Intel::Manageability::Cim::Typed::AMT_EthernetPortSettings>> &EthernetSettings)
 {
-	unsigned int retValue = ERROR_UNKNOWN_ERROR;
+	unsigned int retValue;
 
 	try {
 		if (!m_endpoint)
