197c5b98fa50a4269f9ca598f7fcb02d93c2a665
Alexander Usyskin <alexander.usyskin@intel.com>
2023-01-19 09:20:26 +0200

    LMS: PortForwardingService: disallow copy of LMEConnection
    Define copy constructor and operator as deleted to
    disallow copy of LMEConnection object.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- UNS/PortForwardingService/LMEConnection.h
+++ UNS/PortForwardingService/LMEConnection.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2009-2022 Intel Corporation
+ * Copyright (C) 2009-2023 Intel Corporation
  */
 /*++
 
@@ -245,6 +245,9 @@ class LMEConnection {
 public:
 	LMEConnection(bool verbose = false);
 	~LMEConnection();
+
+	LMEConnection(const LMEConnection&) = delete;
+	LMEConnection& operator = (const LMEConnection&) = delete;
 
 	struct InitParameters
 		{
