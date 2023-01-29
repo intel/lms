5535845afe12a44d8f8c645786b079b7fc7f1069
Alexander Usyskin <alexander.usyskin@intel.com>
2023-01-29 17:38:56 +0200

    LMS: conan: use ACE 7.0.11
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- conanfile.py
+++ conanfile.py
@@ -15,9 +15,9 @@ class LMSConan(ConanFile):
         self.requires("metee/3.1.5@mesw/stable")
         if self.settings.os == "Windows":
             self.requires("libxml2/2.10.3@mesw/stable")
-            self.requires("ACE/7.0.7@mesw/stable")
+            self.requires("ACE/7.0.11@mesw/stable")
         else:
             if os.environ.get("LOCAL_ACE", None):
-                self.requires("ACE/7.0.7@mesw/stable")
+                self.requires("ACE/7.0.11@mesw/stable")
         if self.options.build_tests:
             self.requires("gtest/1.10.0@mesw/stable")
