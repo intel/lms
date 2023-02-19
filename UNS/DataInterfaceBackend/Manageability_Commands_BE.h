05cc24e2425e2c94c0cf92e6fca151cfbb50720d
Alexander Usyskin <alexander.usyskin@intel.com>
2023-02-19 12:31:24 +0200

    LMS: DataInterfaceBackend: drop decalaration of removed method
    Drop GetFeaturesState method declaration as it was removed long ago.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- UNS/DataInterfaceBackend/Manageability_Commands_BE.h
+++ UNS/DataInterfaceBackend/Manageability_Commands_BE.h
@@ -20,7 +20,6 @@ namespace Intel {
 			Manageability_Commands_BE(unsigned int port);
 
 			LMS_ERROR GetTheFeatureState(FEATURES feat, FEATURE_STATE& pState);
-			LMS_ERROR GetFeaturesState(std::vector<FEATURE_STATE> &ppStates);
 			LMS_ERROR GetCustomerType(CUSTOMER_TYPE& pType);
 			LMS_ERROR GetMenageabiltyMode(MENAGEABILTY_MODE& pMode);
 			LMS_ERROR GetFWInfo(std::string &pMEBxVersion, unsigned long &pBiosBootState, bool &pCryptoFuseEnable, bool &pLocalFWupdateEnable);
