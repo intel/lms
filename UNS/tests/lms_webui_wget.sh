96d784edab6119e5cb97a0a041a94d06019d8d4c
Alexander Usyskin <alexander.usyskin@intel.com>
2023-03-19 13:55:11 +0200

    LMS: tests: use non-tls port if tls one is not available
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- UNS/tests/lms_webui_wget.sh
+++ UNS/tests/lms_webui_wget.sh
@@ -2,10 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2019-2023 Intel Corporation
 
+addr=https://127.0.0.1:16993
 #get login page of webui (or 404 error, that is represented by return code 8)
-wget --no-proxy http://127.0.0.1:16993 -O -
+wget --no-proxy $addr -O -
 ret=$?
+if [ $ret -eq 4 ]; then
+ #legacy system without tls
+ addr=http://127.0.0.1:16992
+ wget --no-proxy $addr -O -
+ ret=$?
+fi
 test $ret -eq 8 && exit 0
 test $ret -ne 0 && exit $ret
-wget --no-proxy http://127.0.0.1:16993 -O - | grep -q "Active Management Technology" || exit $?
+wget --no-proxy $addr -O - | grep -q "Active Management Technology" || exit $?
 exit 0
