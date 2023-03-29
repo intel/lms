c7c45f7afd68369d985485cd54644e5c3a29ba53
Alexander Usyskin <alexander.usyskin@intel.com>
2023-03-29 11:08:20 +0300

    LMS: tests: ignore cert in wget test
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- UNS/tests/lms_webui_wget.sh
+++ UNS/tests/lms_webui_wget.sh
@@ -4,15 +4,15 @@
 
 addr=https://127.0.0.1:16993
 #get login page of webui (or 404 error, that is represented by return code 8)
-wget --no-proxy $addr -O -
+wget --no-proxy --no-check-certificate $addr -O -
 ret=$?
 if [ $ret -eq 4 ]; then
  #legacy system without tls
  addr=http://127.0.0.1:16992
- wget --no-proxy $addr -O -
+ wget --no-proxy --no-check-certificate $addr -O -
  ret=$?
 fi
 test $ret -eq 8 && exit 0
 test $ret -ne 0 && exit $ret
-wget --no-proxy $addr -O - | grep -q "Active Management Technology" || exit $?
+wget --no-proxy --no-check-certificate $addr -O - | grep -q "Active Management Technology" || exit $?
 exit 0
