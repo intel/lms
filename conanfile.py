0ffcd6137add1f2ff4b6fcdbb411344d9d1d1a7e
Alexander Usyskin <alexander.usyskin@intel.com>
2023-04-13 18:06:49 +0300

    LMS: conan: pull in libxml2 2.10.4
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class LMSConan(ConanFile):
     def requirements(self):
         self.requires("metee/3.1.6@mesw/stable")
         if self.settings.os == "Windows":
-            self.requires("libxml2/2.10.3@mesw/stable")
+            self.requires("libxml2/2.10.4@mesw/stable")
             self.requires("ACE/7.0.11@mesw/stable")
         else:
             if os.environ.get("LOCAL_ACE", None):
