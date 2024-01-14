d5ef61fd38c3c4ab4b07580d4febd2d08d89f752
Alexander Usyskin <alexander.usyskin@intel.com>
2024-01-14 11:07:56 +0200

    LMS: WsmanClient: EthernetSettingsWSManClient: use catch macro
    Catch more exceptions in centralized way.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- WsmanClient/EthernetSettingsWSManClient.cpp
+++ WsmanClient/EthernetSettingsWSManClient.cpp
@@ -10,6 +10,7 @@
 
 #include "EthernetSettingsWSManClient.h"
 #include "WsmanClientLog.h"
+#include "WsmanClientCatch.h"
 
 EthernetSettingsWSManClient::EthernetSettingsWSManClient(unsigned int port) : BaseWSManClient(port), m_isInit(false)
 {
@@ -21,22 +22,15 @@ EthernetSettingsWSManClient::~EthernetSettingsWSManClient()
 
 unsigned int EthernetSettingsWSManClient::Enumerate(std::vector<std::shared_ptr<Intel::Manageability::Cim::Typed::AMT_EthernetPortSettings>> &EthernetSettings)
 {
-	unsigned int retValue;
-
-	try {
+	try
+	{
 		if (!m_endpoint)
 			SetEndpoint();
 		//Lock WsMan to prevent reentry
 		std::lock_guard<std::mutex> lock(WsManSemaphore());
 		EthernetSettings = Intel::Manageability::Cim::Typed::AMT_EthernetPortSettings::Enumerate(m_client.get());
-		retValue = 0;
 	}
-	catch (std::exception& e)
-	{
-		const char* reason =  e.what();
-		retValue = ERROR_UNKNOWN_ERROR;
-		WSMAN_DEBUG("Error: failed while calling get AMT_EthernetPortSettings: %C\n", reason);
-		m_endpoint = false;
-	}	
-	return retValue;
+	CATCH_exception_return("HBPWSManClient::GetConfigurationInfo")
+
+	return true;
 }
