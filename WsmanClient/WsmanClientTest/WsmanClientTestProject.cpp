aad9c2cf524d595c824d3dc557d053837cf0dfea
Alexander Usyskin <alexander.usyskin@intel.com>
2024-01-17 12:44:14 +0200

    LMS: WsmanClient: Test: make timeset grace 2 sec
    Some platforms are slow to set time and whole
    flow returns after 1 sec.
    Extend grace in test to 2 seconds to cover such case.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- WsmanClient/WsmanClientTest/WsmanClientTestProject.cpp
+++ WsmanClient/WsmanClientTest/WsmanClientTestProject.cpp
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2013-2023 Intel Corporation
+ * Copyright (C) 2013-2024 Intel Corporation
  */
 #include "AMTEthernetPortSettingsClient.h"
 #include "AMTFCFHWSmanClient.h"
@@ -487,7 +487,7 @@ TEST_F(TimeSynchronizationTest, setTime)
 	ASSERT_TRUE(timeClient.GetAMTTime(AMTTime));
 	ASSERT_TRUE(timeClient.SetAMTTime(AMTTime));
 	cout << "Time is now: \t\t" << AMTTime << endl;
-	ASSERT_TRUE(AMTTime-LastAMTTime<=1);
+	ASSERT_TRUE(AMTTime - LastAMTTime <= 2);
 	cout << "Setting forward 100 seconds..." << endl;
 	AMTTime += 100;
 	LastAMTTime = AMTTime;
@@ -495,7 +495,7 @@ TEST_F(TimeSynchronizationTest, setTime)
 	ASSERT_TRUE(timeClient.SetAMTTime(AMTTime));
 	ASSERT_TRUE(timeClient.GetAMTTime(AMTTime));
 	cout << "Time is now: \t\t" << AMTTime << endl;
-	ASSERT_TRUE(AMTTime-LastAMTTime<=1);
+	ASSERT_TRUE(AMTTime - LastAMTTime <= 2);
 }
 
 //Syncs FW time to local time
