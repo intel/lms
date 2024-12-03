09929afc658e7a2c233f30459bc03b39c18f6be1
Alexander Usyskin <alexander.usyskin@intel.com>
2024-12-03 09:30:58 +0200

    LMS: WiFiProfileSyncService: consolidate GetProfileData
    De-duplicate GetProfileData to one function.
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- UNS/WiFiProfileSyncService/WlanProfiles.h
+++ UNS/WiFiProfileSyncService/WlanProfiles.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2018-2021 Intel Corporation
+ * Copyright (C) 2018-2024 Intel Corporation
  */
 #ifndef __WLAN_PRFOFILES_H_
 #define __WLAN_PRFOFILES_H_
@@ -18,30 +18,22 @@ namespace wlanps {
 
 	class WlanProfiles
 	{
-
 	private:
-
-		HANDLE					m_hwlan ;
+		HANDLE m_hwlan;
 
 	public:
-
 		WlanProfiles(HANDLE hwlan);
-
 		~WlanProfiles() {};
 
-		bool GetProfileData(PINTEL_PROFILE_DATA profileData, unsigned long *pProfileFlags);
-
+		bool GetProfileData(PINTEL_PROFILE_DATA profileData, unsigned long* pProfileFlags,
+			const authenticationSet_t& supportedAuthentication, const encriptionSet_t& supportedEncription);
 		bool GetProfiles(WlanOsProfileList &profiles, const authenticationSet_t &supportedAuthentication, const encriptionSet_t &supportedEncription);
 
 	private:
 		std::wstring xmlRead(const std::wstring &strXML, std::wstring strKey);
-
 		bool isLegalProfileName(const std::wstring &profileName);
-
 		bool isSupportedEncription(const std::wstring &enc, const encriptionSet_t &supportedEnc);
-
 		bool isSupportedAuthentication(const std::wstring &auth, const authenticationSet_t &supportedAuth);
 	};
 }
-
 #endif //__WLAN_PRFOFILES_H_
